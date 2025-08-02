899d8823ca7ff4264a3869ec92e96a83168060be
Adam Duehring <duehring.adam@gmail.com>
2025-08-02 14:34:15 -0400

    fix door open

--- door_open.gd
+++ door_open.gd
@@ -4,6 +4,8 @@ extends Node
 @onready var animation_player: AnimationPlayer = $"../../../../AnimationPlayer"
 @onready var scene_transition_component: SceneTransitionComponent = $"../../../../SceneTransitionComponent"
 
+var used: bool = false
+
 func _on_open_state_entered() -> void:
 	if not animation_player.is_playing():
 		animation_player.play("open")
@@ -12,7 +14,8 @@ func _on_open_state_exited() -> void:
 	pass
 
 func _on_open_state_processing(delta: float) -> void:
-	if Input.is_action_just_pressed("use"):
+	if Input.is_action_just_pressed("use") && !used:
+		used = true
 		Global.player.state_chart.send_event("door")
 		get_tree().create_timer(1).timeout.connect(func():scene_transition_component.transition())
 		
